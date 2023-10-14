8060ea4c61321620fc3e255aedec19c23e638b62
xiekun1992 <xkfront@outlook.com>
2023-10-15 02:07:58 +0800

    refactor: add linux config for cmake

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 mkdir -p ./build/
 cd ./build
 rm -rf ./Debug/
+
+if [[ $OSTYPE == "linux-gnu" ]]; then
+  sudo apt install libxtst-dev libx11-dev build-essential cmake -y
+# elif [[ $OSTYPE == "msys" ]]; then
+fi
+
 cmake -B . -S ..
 cmake --build .
 
-cp -f ./Debug/libcapture.dll ./Debug/libcapture.lib /e/Desktop/hello/
-cp -f ./Debug/libcapture.dll ./Debug/libcapture.lib /e/Desktop/lctrl_rust/
+if [[ $OSTYPE == "linux-gnu" ]]; then
+  mv ./liblibcapture.so ./libcapture.so
+  cp -f ./libcapture.so ../../lctrl_rust/
+elif [[ $OSTYPE == "msys" ]]; then
+  # cp -f ./Debug/libcapture.dll ./Debug/libcapture.lib /e/Desktop/hello/
+  cp -f ./Debug/libcapture.dll ./Debug/libcapture.lib ../../lctrl_rust/
+fi
--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,7 @@
 #include "utils.h"
 
 DLL_EXPORT void utils_open(wchar_t *path) {
+#if _WIN32 == 1
   setlocale(LC_ALL, "");
   SHELLEXECUTEINFO info = { 0 };
   info.cbSize = sizeof(info);
@@ -8,9 +9,14 @@ DLL_EXPORT void utils_open(wchar_t *path) {
   info.lpFile = L"explorer.exe";
   info.lpParameters = path;
   ShellExecuteEx(&info);
+#elif __linux == 1
+#endif
 }
 
 DLL_EXPORT void get_screen_size(int size[2]) {
+#if _WIN32 == 1
   size[0] = GetSystemMetrics(SM_CXSCREEN);
   size[1] = GetSystemMetrics(SM_CYSCREEN);
+#elif __linux == 1
+#endif
 }
