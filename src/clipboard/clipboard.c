6041520e87d402d3edbe40f70037765a06e6604a
xiekun1992 <xkfront@outlook.com>
2024-04-24 02:09:25 +0800

    feat: add clipboard apis

--- src/clipboard/clipboard.c
+++ src/clipboard/clipboard.c
@@ -1,86 +1,96 @@
 #include "clipboard.h"
 
-struct Clipboard context;
+struct Clipboard clipboard_context;
 
-DLL_EXPORT void clipboard_init() {
-  context.content = (wchar_t*)malloc(sizeof(wchar_t) * 1024 * 1024);
-  context.notify = false;
+DLL_EXPORT void clipboard_init()
+{
+    clipboard_context.content = (wchar_t *)malloc(sizeof(wchar_t) * 1024 * 1024);
+    clipboard_context.notify = false;
 }
 
-DLL_EXPORT void clipboard_dispose() {
-  free(context.content);
+DLL_EXPORT void clipboard_dispose()
+{
+    free(clipboard_context.content);
 }
 
 #if _WIN32 == 1
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-	switch (message)
-	{
-	case WM_CLIPBOARDUPDATE:
-    // handle change 
-    context.fn();
-		break;
-	// case WM_PAINT:
-	// {
-	// 	PAINTSTRUCT ps;
-	// 	HDC hdc = BeginPaint(hWnd, &ps);
-	// 	EndPaint(hWnd, &ps);
-	// }
-	// break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		RemoveClipboardFormatListener(hWnd);
-		break;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-	return 0;
-}
-#endif
-
-DLL_EXPORT bool write_text(wchar_t* text) {
-  bool res = false;
-#if _WIN32 == 1
-  OpenClipboard(0);
-  HGLOBAL clipbuffer = GlobalAlloc(GMEM_MOVEABLE, (wcslen(text) + 1) * sizeof(wchar_t));
-  if (clipbuffer != NULL) {
-    wchar_t* buffer = (wchar_t*)GlobalLock(clipbuffer);
-    if (buffer != NULL) {
-      EmptyClipboard();
-      wcscpy(buffer, text);
-      SetClipboardData(CF_UNICODETEXT, clipbuffer);
-
-      GlobalUnlock(clipbuffer);
-      res = true;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CLIPBOARDUPDATE:
+        // handle change
+        clipboard_context.fn();
+        break;
+    // case WM_PAINT:
+    // {
+    // 	PAINTSTRUCT ps;
+    // 	HDC hdc = BeginPaint(hWnd, &ps);
+    // 	EndPaint(hWnd, &ps);
+    // }
+    // break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        RemoveClipboardFormatListener(hWnd);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
     }
-  }
-  CloseClipboard();
+    return 0;
+}
 #endif
-  return res;
+
+DLL_EXPORT bool write_text(wchar_t *text)
+{
+    bool res = false;
+#if _WIN32 == 1
+    OpenClipboard(0);
+    HGLOBAL clipbuffer = GlobalAlloc(GMEM_MOVEABLE, (wcslen(text) + 1) * sizeof(wchar_t));
+    if (clipbuffer != NULL)
+    {
+        wchar_t *buffer = (wchar_t *)GlobalLock(clipbuffer);
+        if (buffer != NULL)
+        {
+            EmptyClipboard();
+            wcscpy(buffer, text);
+            SetClipboardData(CF_UNICODETEXT, clipbuffer);
+
+            GlobalUnlock(clipbuffer);
+            res = true;
+        }
+    }
+    CloseClipboard();
+#endif
+    return res;
 }
 
-DLL_EXPORT wchar_t* read_text() {
-  wchar_t *empty = L"";
+DLL_EXPORT wchar_t *read_text()
+{
+    wchar_t *empty = L"";
 #if _WIN32 == 1
-  OpenClipboard(0);
+    OpenClipboard(0);
 
-  if (!IsClipboardFormatAvailable(CF_UNICODETEXT)) {
+    if (!IsClipboardFormatAvailable(CF_UNICODETEXT))
+    {
+        return empty;
+    }
+
+    HANDLE handle = (HANDLE)GetClipboardData(CF_UNICODETEXT);
+    if (handle != NULL)
+    {
+        wchar_t *buffer = GlobalLock(handle);
+        if (buffer != NULL)
+        {
+            wcscpy(clipboard_context.content, buffer);
+
+            GlobalUnlock(handle);
+            CloseClipboard();
+            return clipboard_context.content;
+        }
+    }
+    CloseClipboard();
+#endif
     return empty;
-  }
-
-  HANDLE handle = (HANDLE)GetClipboardData(CF_UNICODETEXT);
-  if (handle != NULL) {
-    wchar_t* buffer = GlobalLock(handle);
-    if (buffer != NULL) {
-      wcscpy(context.content, buffer);
-
-      GlobalUnlock(handle);
-      CloseClipboard();
-      return context.content;
-    }
-  }
-  CloseClipboard();
-#endif
-  return empty;
 }
 
 // bool write_files(std::vector<std::u16string> files) {
@@ -96,7 +106,7 @@ DLL_EXPORT wchar_t* read_text() {
 //   DROPFILES dobj = { 20, { 0, 0 }, 0, 1 };
 //   int nLen = (sFiles.size() + 1) * sizeof(char16_t);
 //   int nGblLen = sizeof(dobj) + nLen;
-  
+
 //   HGLOBAL hGbl = GlobalAlloc(GMEM_ZEROINIT | GMEM_MOVEABLE | GMEM_DDESHARE, nGblLen);
 //   if (hGbl != NULL) {
 //     char* buffer = (char*)GlobalLock(hGbl);
@@ -146,77 +156,83 @@ DLL_EXPORT wchar_t* read_text() {
 //   return filenames;
 // }
 
-DLL_EXPORT void capture(void (*fn)(void)) {
-  context.fn = fn;
+DLL_EXPORT void capture(void (*fn)(void))
+{
+    clipboard_context.fn = fn;
 
 #if _WIN32 == 1
-  const wchar_t CLASS_NAME[] = L"Clipboard Window Class";
+    const wchar_t CLASS_NAME[] = L"Clipboard Window Class";
 
-  HINSTANCE hInstance = GetModuleHandle(0);
-  WNDCLASS wc = { 0 };
-  wc.lpfnWndProc = WndProc;
-  wc.hInstance = hInstance;
-  wc.lpszClassName = CLASS_NAME;
+    HINSTANCE hInstance = GetModuleHandle(0);
+    WNDCLASS wc = {0};
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
 
-  RegisterClass(&wc);
-  HWND hwnd = CreateWindowEx(
-    0,                              // Optional window styles.
-    CLASS_NAME,                     // Window class
-    L"Clipboard Listener Windows",    // Window text
-    WS_OVERLAPPEDWINDOW,            // Window style
-    // Size and position
-    CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-    NULL,       // Parent window    
-    NULL,       // Menu
-    hInstance,  // Instance handle
-    NULL        // Additional application data
-  );
-  if (hwnd != NULL) {
-    AddClipboardFormatListener(hwnd);
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0)) {
-      TranslateMessage(&msg);
-      DispatchMessage(&msg);
+    RegisterClass(&wc);
+    HWND hwnd = CreateWindowEx(
+        0,                             // Optional window styles.
+        CLASS_NAME,                    // Window class
+        L"Clipboard Listener Windows", // Window text
+        WS_OVERLAPPEDWINDOW,           // Window style
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL,      // Parent window
+        NULL,      // Menu
+        hInstance, // Instance handle
+        NULL       // Additional application data
+    );
+    if (hwnd != NULL)
+    {
+        AddClipboardFormatListener(hwnd);
+        MSG msg;
+        while (GetMessage(&msg, NULL, 0, 0))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
     }
-  }
 #elif __linux == 1
-  notify = true;
-  Display *display = XOpenDisplay(NULL);
-  unsigned long color = BlackPixel(display, DefaultScreen(display));
-  Window window = XCreateSimpleWindow(display, DefaultRootWindow(display), 0,0, 1,1, 0, color, color);
-  
-  const char* bufname_clipboard = "CLIPBOARD";
-  const char* bufname_primary = "PRIMARY";
-  int event_base, error_base;
-  XEvent event;
-  Atom bufid_clip = XInternAtom(display, bufname_clipboard, False);
-  Atom bufid_pri = XInternAtom(display, bufname_primary, False);
+    notify = true;
+    Display *display = XOpenDisplay(NULL);
+    unsigned long color = BlackPixel(display, DefaultScreen(display));
+    Window window = XCreateSimpleWindow(display, DefaultRootWindow(display), 0, 0, 1, 1, 0, color, color);
 
-  XFixesQueryExtension(display, &event_base, &error_base);
-  XFixesSelectSelectionInput(display, DefaultRootWindow(display), bufid_clip, XFixesSetSelectionOwnerNotifyMask);
-  XFixesSelectSelectionInput(display, DefaultRootWindow(display), bufid_pri, XFixesSetSelectionOwnerNotifyMask);
+    const char *bufname_clipboard = "CLIPBOARD";
+    const char *bufname_primary = "PRIMARY";
+    int event_base, error_base;
+    XEvent event;
+    Atom bufid_clip = XInternAtom(display, bufname_clipboard, False);
+    Atom bufid_pri = XInternAtom(display, bufname_primary, False);
 
-  while (notify) {
-    if (XPending(display)) {
-      XNextEvent(display, &event);
-      if (
-        event.type == event_base + XFixesSelectionNotify && 
-        (((XFixesSelectionNotifyEvent*)&event)->selection == bufid_clip || ((XFixesSelectionNotifyEvent*)&event)->selection == bufid_pri)
-      ) {
-        // printf("update\n");
-        lambda_update_handler();
-      }
+    XFixesQueryExtension(display, &event_base, &error_base);
+    XFixesSelectSelectionInput(display, DefaultRootWindow(display), bufid_clip, XFixesSetSelectionOwnerNotifyMask);
+    XFixesSelectSelectionInput(display, DefaultRootWindow(display), bufid_pri, XFixesSetSelectionOwnerNotifyMask);
+
+    while (notify)
+    {
+        if (XPending(display))
+        {
+            XNextEvent(display, &event);
+            if (
+                event.type == event_base + XFixesSelectionNotify &&
+                (((XFixesSelectionNotifyEvent *)&event)->selection == bufid_clip || ((XFixesSelectionNotifyEvent *)&event)->selection == bufid_pri))
+            {
+                // printf("update\n");
+                lambda_update_handler();
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(1)); // prevent high cpu usage
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(1)); // prevent high cpu usage
-  }
 
-  XDestroyWindow(display, window);
-  XCloseDisplay(display);
+    XDestroyWindow(display, window);
+    XCloseDisplay(display);
 #endif
 }
 
-DLL_EXPORT void release() {
+DLL_EXPORT void release()
+{
 #if __linux == 1
-  context.notify = false;
+    clipboard_context.notify = false;
 #endif
 }
